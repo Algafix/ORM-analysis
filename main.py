6685e4c15db75305c977eae2bc70bae2c3637178
Aleix <algafix@gmail.com>
2020-12-19 16:50:51 +0100

    retrieving headers first approach

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import MySQLdb
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def getDBs():
 
+    db_orm = MySQLdb.connect(host="127.0.0.1", port = 3306, user="root", passwd="pinacolada", db="ORM")
+    db_mod = MySQLdb.connect(host="127.0.0.1", port = 3307, user="root", passwd="pinacolada", db="ORM")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    return db_orm, db_mod
 
+def getUrls(db, domain):
 
-# Press the green button in the gutter to run the script.
+    cursor = db.cursor()
+    query = f"SELECT url.hostname, url.domain, url.headers " \
+            f"FROM ORM.url as url, ORM.domain as domain " \
+            f"WHERE domain.id = url.domain AND domain.name = '{domain}'"
+    cursor.execute(query)
+    query_result = cursor.fetchall()
+    cursor.close()
+
+    return query_result
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    db_orm, db_mod = getDBs()
+
+    result = getUrls(db_orm, "google.com")
+
+    for row in result:
+        print(row)
+
+    exit(1)
+
+
+
